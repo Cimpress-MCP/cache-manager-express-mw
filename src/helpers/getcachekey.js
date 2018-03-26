4a208a519fc3590cbeff83125787511783c45a03
Anthony Frasso <anf4@cornell.edu>
2018-03-26 15:41:21 -0400

    Supporting request headers in cache key. (#13)

--- src/helpers/getcachekey.js
+++ src/helpers/getcachekey.js
@@ -1,6 +1,6 @@
 const _ = require("lodash");
 
-const getCacheKey = function({ request, options: { prefix, defaults } = {} }) {
+const getCacheKey = function({ request, options: { prefix, headers, defaults } = {} }) {
   let cachePrefix = prefix;
   if (cachePrefix) {
     cachePrefix = _.trim(cachePrefix);
@@ -11,6 +11,8 @@ const getCacheKey = function({ request, options: { prefix, defaults } = {} }) {
     cachePrefix = "";
   }
 
+  const headerInfix = getHeaderInfix(request, headers);
+
   let sortedQueryString = "";
   if (request.query) {
     let query = _.omitBy(request.query, _.isNil);
@@ -20,8 +22,26 @@ const getCacheKey = function({ request, options: { prefix, defaults } = {} }) {
     }
   }
 
-  const cacheKey = `${cachePrefix}${request.method}:${request.path}${sortedQueryString}`;
+  const cacheKey = `${cachePrefix}${request.method}${headerInfix}:${request.path}${sortedQueryString}`;
   return cacheKey;
+};
+
+const getHeaderInfix = function(request, headers) {
+  if (!headers) {
+    return "";
+  }
+
+  if (_.isString(headers)) {
+    headers = [ headers ];
+  }
+
+  let headerInfix = _(headers)
+    .sort()
+    .map(header => _.toLower(header))
+    .map(header => `${header}:${request.get(header)}`)
+    .join(":");
+
+  return `:${headerInfix}`;
 };
 
 module.exports = getCacheKey;
